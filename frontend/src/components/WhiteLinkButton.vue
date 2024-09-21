6599d9756be6e0059e585b5b0ee27890a2ebb983
DMITRII1548 <korsunov2006@list.ru>
2024-09-21 15:38:54 +0600

    Created home page

---
+++ frontend/src/components/WhiteLinkButton.vue
@@ -0,0 +1,12 @@
+<template>
+    <router-link :to="to" class="px-2 py-1 rounded-lg border-white border-2 bg-slate-50 text-lg font-semibold hover:bg-white"><slot></slot></router-link>
+</template>
+
+<script setup>
+const props = defineProps({
+  to: {
+    type: [String, Object],
+    required: true,
+  }
+});
+</script>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,8 +1,16 @@
 <template>
-    <main>
-        <h1>The best task manager</h1>
+    <main class="w-screen h-screen flex justify-center items-center" style="background: url('/imgs/home-bg.avif');  background-size: cover;">
+        <div class="block-opacity-50 p-4 flex flex-col justify-center items-center gap-3">
+            <div class="z-10">
+                <h1 class="text-white text-7xl text-center font-semibold">The best task manager</h1>
+            </div>
+            <div class="z-10 flex justify-center">
+                <WhiteLinkButton to="/">Sign in</WhiteLinkButton>
+            </div>
+        </div>
     </main>
 </template>
 
-<script>
+<script setup>
+import WhiteLinkButton from '../components/WhiteLinkButton.vue'
 </script>
