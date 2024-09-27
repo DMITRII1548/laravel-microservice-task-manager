04cda7cc481d8bdb07c19feefdf5868a7414b655
DMITRII1548 <korsunov2006@list.ru>
2024-09-27 17:19:31 +0600

    Added ability to changing status

--- frontend/src/components/Task/Details.vue
+++ frontend/src/components/Task/Details.vue
@@ -64,13 +64,13 @@
                             <span>{{ task.created_at ?? '-' }}</span>
                         </div>
                         <div class="col-span-2 flex justify-between gap-2">
-                            <button class="flex items-center gap-1 bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded">
+                            <button :disabled="isUpdating" @click="store.dispatch('changeToBackStatusTask', task.id)" class="flex items-center gap-1 bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
                                 </svg>
                                 <span>Back status</span>
                             </button>
-                            <button class="flex items-center gap-1 bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded">
+                            <button :disabled="isUpdating" @click="store.dispatch('changeToNextStatusTask', task.id)" class="flex items-center gap-1 bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded">
                                 <span>Next status</span>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
@@ -85,7 +85,10 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
+import { useStore } from 'vuex'
+
+const store = useStore()
 
 const props = defineProps({
     task: {
@@ -107,4 +110,8 @@ const openModal = () => {
 const closeModal = () => {
     isModalOpen.value = false
 }
+
+const isUpdating = computed(() => {
+    return store.getters.isUpdatingTask
+})
 </script>
--- frontend/src/store/modules/task.js
+++ frontend/src/store/modules/task.js
@@ -2,12 +2,14 @@ import API from '../../api'
 
 const state = {
     tasks: [],
+    task: {},
     taskErrors: {
         title: '',
         content: '',
         tags: ''
     },
-    isLoadingTask: false
+    isLoadingTask: false,
+    isUpdatingTask: false
 }
 
 const actions = {
@@ -19,9 +21,21 @@ const actions = {
                 let tasks = Array.from(new Set([...existingTasks, ...res.data]))
                 tasks = tasks.filter((task, index, self) =>
                     index === self.findIndex(t => t.id === task.id)
-                );
+                )
 
                 commit('setTasks', tasks)
+
+                return res
+            })
+            .catch(e => {
+                throw e
+            })
+    },
+
+    async getTask({ commit }, id) {
+        return API.get(`/users/tasks/${id}`)
+            .then(res => {
+                commit('setTask', res.data)
 
                 return res
             })
@@ -67,19 +81,73 @@ const actions = {
                 commit('setTasks', [])
                 dispatch('getTasks')
             })
+    },
+
+    async changeToNextStatusTask({ dispatch, getters, commit }, id) {
+        commit('setIsUpdatingTask', true)
+        return API.patch(`/users/tasks/${id}/status/next`)
+            .then(async (res) => {
+                await dispatch('getTask', id)
+
+                const updatedTask = getters.task
+                console.log(updatedTask)
+                
+                const tasks = getters.tasks.map(task => 
+                    task.id === updatedTask.id ? updatedTask : task
+                )
+
+                commit('setTasks', tasks)
+                commit('setIsUpdatingTask', false)
+
+                return res
+            })
+            .catch(e => {
+                commit('setIsUpdatingTask', false)
+                throw e
+            })
+    },
+
+    async changeToBackStatusTask({ dispatch, getters, commit }, id) {
+        commit('setIsUpdatingTask', true)
+        return API.patch(`/users/tasks/${id}/status/back`)
+            .then(async (res) => {
+                await dispatch('getTask', id)
+
+                const updatedTask = getters.task
+                console.log(updatedTask)
+                
+                const tasks = getters.tasks.map(task => 
+                    task.id === updatedTask.id ? updatedTask : task
+                )
+
+                commit('setTasks', tasks)
+                commit('setIsUpdatingTask', false)
+
+                return res
+            })
+            .catch(e => {
+                commit('setIsUpdatingTask', false)
+                throw e
+            })
     }
 }
 
 const getters = {
     tasks: state => state.tasks,
+    task: state => state.task,
 
     titleTaskError: state => state.taskErrors.title,
     contentTaskError: state => state.taskErrors.content,
     tagsTaskError: state => state.taskErrors.tags,
-    isLoadingTask: state => state.isLoadingTask
+    isLoadingTask: state => state.isLoadingTask,
+    isUpdatingTask: state => state.isUpdatingTask
 }
 
 const mutations = {
+    setTask(state, task) {
+        state.task = task
+    },
+
     setTasks(state, tasks) {
         state.tasks = tasks
     },
@@ -98,6 +166,10 @@ const mutations = {
 
     setIsloadingTask(state, value) {
         state.isLoadingTask = value
+    },
+
+    setIsUpdatingTask(state, value) {
+        state.isUpdatingTask = value
     }
 }
 
